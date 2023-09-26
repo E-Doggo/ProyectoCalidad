bb9ee59f02939b9ad001f1e78ff75485f18fadef
Victor Montano <victorm@sintesis.com.bo>
2023-09-26 13:47:07 -0400

    refactor de directions_to_move

--- utils.py
+++ utils.py
@@ -20,79 +20,106 @@ def get_opponent(player):
 
 # Define the function for making a move on the board
 
-def directions_to_move(direction, new_board, row_copy, col_copy, previous_position, player):#no
-    if direction == 'NW':
-        for n in range(1, min(row_copy+1, col_copy+1)+1):
-            if row_copy-n >= 0 and col_copy >= 0:
-                if new_board[row_copy-n][col_copy-n] is not None:
-                    break
-                new_board[row_copy-n][col_copy-n] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy-n, col_copy-n]
-
-    elif direction == 'N':
-        for n in range(1, row_copy+1):
-            if row_copy-n >= 0:
-                if new_board[row_copy-n][col_copy] is not None:
-                    break
-                new_board[row_copy-n][col_copy] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy-n, col_copy]
-
-    elif direction == 'NE':
-        for n in range(1, min(row_copy+1, 4-col_copy)+1):
-            if row_copy-n >= 0 and col_copy+n < 4:
-                if new_board[row_copy-n][col_copy+n] is not None:
-                    break
-                new_board[row_copy-n][col_copy+n] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy-n, col_copy+n]
-
-    elif direction == 'W':
-        for n in range(1, col_copy+1):
-            if col_copy-n >= 0:
-                if new_board[row_copy][col_copy-n] is not None:
-                    break
-                new_board[row_copy][col_copy-n] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy, col_copy-n]
-
-    elif direction == 'E':
-        for n in range(1, 4-col_copy):
-            if col_copy+n < 4:
-                if new_board[row_copy][col_copy+n] is not None:
-                    break
-                new_board[row_copy][col_copy+n] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy, col_copy+n]
-
-    elif direction == 'SW':
-        for n in range(1, min(4-row_copy, col_copy+1)+1):
-            if row_copy+n < 4 and col_copy-n > 0:
-                if new_board[row_copy+n][col_copy-n] is not None:
-                    break
-                new_board[row_copy+n][col_copy-n] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy+n, col_copy-n]
-
-    elif direction == 'S':
-        for n in range(1, 4-row_copy):
-            if row_copy+n < 4:
-                if new_board[row_copy+n][col_copy] is not None:
-                    break
-                new_board[row_copy+n][col_copy] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy+n, col_copy]
-
-    elif direction == 'SE':
-        for n in range(1, min(4-row_copy, 4-col_copy)+1):
-            if row_copy+n < 4 and col_copy+n < 4:
-                if new_board[row_copy+n][col_copy+n] is not None:
-                    break
-                new_board[row_copy+n][col_copy+n] = player
-                new_board[previous_position[0]][previous_position[1]] = None
-                previous_position = [row_copy+n, col_copy+n]
+def move_northwest(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, min(row_copy + 1, col_copy + 1) + 1):
+        if row_copy - n >= 0 and col_copy >= 0:
+            if new_board[row_copy - n][col_copy - n] is not None:
+                break
+            new_board[row_copy - n][col_copy - n] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy - n, col_copy - n]
     return new_board
+
+def move_north(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, row_copy + 1):
+        if row_copy - n >= 0:
+            if new_board[row_copy - n][col_copy] is not None:
+                break
+            new_board[row_copy - n][col_copy] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy - n, col_copy]
+    return new_board
+
+def move_northeast(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, min(row_copy + 1, 4 - col_copy) + 1):
+        if row_copy - n >= 0 and col_copy + n < 4:
+            if new_board[row_copy - n][col_copy + n] is not None:
+                break
+            new_board[row_copy - n][col_copy + n] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy - n, col_copy + n]
+    return new_board
+
+def move_west(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, col_copy + 1):
+        if col_copy - n >= 0:
+            if new_board[row_copy][col_copy - n] is not None:
+                break
+            new_board[row_copy][col_copy - n] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy, col_copy - n]
+    return new_board
+
+def move_east(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, 4 - col_copy):
+        if col_copy + n < 4:
+            if new_board[row_copy][col_copy + n] is not None:
+                break
+            new_board[row_copy][col_copy + n] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy, col_copy + n]
+    return new_board
+
+def move_southwest(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, min(4 - row_copy, col_copy + 1) + 1):
+        if row_copy + n < 4 and col_copy - n >= 0:
+            if new_board[row_copy + n][col_copy - n] is not None:
+                break
+            new_board[row_copy + n][col_copy - n] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy + n, col_copy - n]
+    return new_board
+
+def move_south(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, 4 - row_copy):
+        if row_copy + n < 4:
+            if new_board[row_copy + n][col_copy] is not None:
+                break
+            new_board[row_copy + n][col_copy] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy + n, col_copy]
+    return new_board
+
+def move_southeast(new_board, row_copy, col_copy, previous_position, player):
+    for n in range(1, min(4 - row_copy, 4 - col_copy) + 1):
+        if row_copy + n < 4 and col_copy + n < 4:
+            if new_board[row_copy + n][col_copy + n] is not None:
+                break
+            new_board[row_copy + n][col_copy + n] = player
+            new_board[previous_position[0]][previous_position[1]] = None
+            previous_position = [row_copy + n, col_copy + n]
+    return new_board
+
+def directions_to_move(direction, new_board, row_copy, col_copy, previous_position, player):
+    if direction == 'NW':
+        return move_northwest(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'N':
+        return move_north(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'NE':
+        return move_northeast(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'W':
+        return move_west(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'E':
+        return move_east(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'SW':
+        return move_southwest(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'S':
+        return move_south(new_board, row_copy, col_copy, previous_position, player)
+    elif direction == 'SE':
+        return move_southeast(new_board, row_copy, col_copy, previous_position, player)
+    else:
+        return new_board
+
 
 
 def make_move(board, move, player):

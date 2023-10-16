a997fbe1314e6846dece2977ab9d270eb6ce6882
E-Doggo <rodrigo.guardia316@gmail.com>
2023-10-16 13:09:52 -0400

    testint check_win

--- game_test.py
+++ game_test.py
@@ -19,3 +19,33 @@ def create_state():
 def test_create_board():
     board = create_board()
     assert board == [['B',None,None,'W'],[None,'B', 'W', None],[None, 'W', 'B', None],['W', None, None, 'B']]
+
+def test_check_win_1(empty_board):
+    board = empty_board
+    for i in range(4):
+        board [0][i] = BLACK
+    result = check_win(board, BLACK)
+    assert result == True
+
+
+def test_check_win_2(empty_board):
+    board = empty_board
+    for i in range(4):
+        board [i][0] = BLACK
+    result = check_win(board, BLACK)
+    assert result == True
+
+def test_check_win_3(empty_board):
+    board = empty_board
+    board [0][0] = BLACK
+    board [0][3] = BLACK  
+    board [3][0] = BLACK
+    board [3][3] = BLACK
+    result = check_win(board, BLACK)
+    assert result == True
+
+
+def test_check_win_4(empty_board):
+    board = empty_board
+    result = check_win(board, BLACK)
+    assert result == False

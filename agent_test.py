51386a1171e1d59d701a361cd92a6c0c625ab71f
Victor Montano <victorm@sintesis.com.bo>
2023-10-16 23:26:41 -0400

    terminal Test: Segundo test

--- agent_test.py
+++ agent_test.py
@@ -18,3 +18,10 @@ def test_terminal_test_horizontal_winner(empty_board, player_white):
     # Configura un ganador en una fila horizontal
     board[0] = [player_white, player_white, player_white, player_white]
     assert terminal_test(board, player_white) == True
+
+def test_terminal_test_vertical_winner(empty_board, player_white, player_black):
+    board = empty_board
+    # Configura un ganador en una columna vertical
+    for i in range(4):
+        board[i][0] = player_black
+    assert terminal_test(board, player_black) == True

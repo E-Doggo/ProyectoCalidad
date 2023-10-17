49ba63705ea45b8b5e05691699ba54e03458ca8b
Victor Montano <victorm@sintesis.com.bo>
2023-10-16 23:23:00 -0400

    terminalTest:PrimerTest

---
+++ agent_test.py
@@ -0,0 +1,20 @@
+from agent import *
+import pytest 
+
+@pytest.fixture
+def empty_board():
+    return [[None for _ in range(4)] for _ in range(4)]
+
+@pytest.fixture
+def player_white():
+    return WHITE
+
+@pytest.fixture
+def player_black():
+    return BLACK
+
+def test_terminal_test_horizontal_winner(empty_board, player_white):
+    board = empty_board
+    # Configura un ganador en una fila horizontal
+    board[0] = [player_white, player_white, player_white, player_white]
+    assert terminal_test(board, player_white) == True
